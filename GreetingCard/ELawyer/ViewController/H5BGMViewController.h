facc76fb1b9de165a7c586587898411752eb3e31
3738799heman <wataxiwaheman@gmail.com>
2015-10-31 00:26:40 +0800

    H5BGM

---
+++ GreetingCard/ELawyer/ViewController/H5BGMViewController.h
@@ -0,0 +1,13 @@
+//
+//  H5BGMViewController.h
+//  ELawyer
+//
+//  Created by 鹤楠 on 15/10/31.
+//  Copyright (c) 2015年 binfenfuture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface H5BGMViewController : UIViewController
+
+@end
---
+++ GreetingCard/ELawyer/ViewController/H5BGMViewController.m
@@ -0,0 +1,37 @@
+//
+//  H5BGMViewController.m
+//  ELawyer
+//
+//  Created by 鹤楠 on 15/10/31.
+//  Copyright (c) 2015年 binfenfuture. All rights reserved.
+//
+
+#import "H5BGMViewController.h"
+
+@interface H5BGMViewController ()
+
+@end
+
+@implementation H5BGMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
